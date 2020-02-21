84eb0cd71409f066dcbaab8798b51d114a815762
johan <joaramirezra@unal.edu.co>
2020-02-21 16:58:44 -0500

    funcina los tres sensores DHT22

--- firmware/ProgramacionESP/DHT22/DHT22.ino
+++ firmware/ProgramacionESP/DHT22/DHT22.ino
@@ -1,45 +1,67 @@
 // librerias necesarias 
 #include "DHT.h"
 
-// Definiciones pines DTH11
-#define DHTPIN D4     // Digital pin connected to the DHT sensor
-#define DHTTYPE DHT22   // DHT 11
+// Definicion de pines y tipo de sensor
 
-//Definicion pines conectado al FC-28
-const int PinSensor = D4;
-const int ActivacionSensor = D0;
+#define DHTTYPE DHT22   // DHT 22
+#define Sensor1 D5
+#define Sensor2 D6
+#define Sensor3 D4
 
-//Variables a ser medidas
-int humedadTierra;
-float humedadAire, temperaturaAire;
+// Prototipos Funciones
+void configurarDHT22();
+float ValidadValor(float valor);
+float MedirHumedad(byte sensor);
+float MedirTemperatura(byte sensor);
+void imprimitHumedadYTemperatura(byte i);
+// Se inicializan los ohbjetos 
 
-// variables de validacion
-char input;
-
-// creacion sw objeto tipo dth 
-DHT dht(DHTPIN, DHTTYPE);
-
+DHT dht1(Sensor1,DHTTYPE);
+DHT dht2(Sensor2,DHTTYPE);
+DHT dht3(Sensor3,DHTTYPE);
 
 void setup() {
-  Serial.begin(9600); 
-  dht.begin();  
-  humedadAire = 0;
-  temperaturaAire = 0;
-  delay(2000); // tiempo de incializacion de sensor 
-  
+  configurarDHT22();
+  Serial.begin(9600);
 }
 
 void loop() {
-        delay(2000);
-        humedadAire = dht.readHumidity();
-        temperaturaAire = dht.readTemperature(); // se leen los valores en celcius
-        if (isnan(humedadAire) || isnan(temperaturaAire)) {
-            Serial.println(F("Failed to read from DHT sensor!"));
-            return;
-          }
-        Serial.print("%, Humedad Aire: "); 
-        Serial.print(humedadAire);
-        Serial.print("%, Temperatura Aire: "); 
-        Serial.print(temperaturaAire);
-        Serial.println(" °C");
+  for(int i=1;i<=3;i++){
+    imprimitHumedadYTemperatura(i);
+    delay(2000);
+  }
 }
+
+void imprimitHumedadYTemperatura(byte i){
+  Serial.print("Sensor");
+  Serial.print(i);
+  Serial.print(" Humedad: ");
+  Serial.print(MedirHumedad(i));
+  Serial.print(" Temperatura: ");
+  Serial.println(MedirTemperatura(i));
+}
+void configurarDHT22(){
+  dht1.begin();
+  dht2.begin();
+  dht3.begin();
+}
+
+float ValidadValor(float valor){
+  if (isnan(valor)) return 0;
+  return valor;
+}
+
+float MedirHumedad(byte sensor){
+  if(sensor == 1) return ValidadValor(dht1.readHumidity());
+  else if(sensor == 2) return ValidadValor(dht2.readHumidity());
+  else if(sensor == 3) return ValidadValor(dht3.readHumidity());
+  else return 0;
+}
+ 
+float MedirTemperatura(byte sensor){
+  if(sensor == 1) return ValidadValor(dht1.readTemperature());
+  else if(sensor == 2) return ValidadValor(dht2.readTemperature());
+  else if(sensor == 3) return ValidadValor(dht3.readTemperature());
+  else return 0;
+}
+  
