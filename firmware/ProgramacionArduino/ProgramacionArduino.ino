551de995b7f3a6f2e6169107fa10f01665d0c3b7
johan <joaramirezra@unal.edu.co>
2020-02-25 20:13:44 -0500

    Version 0.8 firmware

--- firmware/ProgramacionArduino/ProgramacionArduino.ino
+++ firmware/ProgramacionArduino/ProgramacionArduino.ino
@@ -61,12 +61,12 @@ void loop() {
   else if (accion == 0){Serial.print("Apagar");Serial.println(numero);}
 }
 
-void configurarActuadores(){
-  // Ventiladores pines {D:4,5,6,7,} Recamara Superior             // 4
-  // Ventiladores pines {D:8,9} Entrada General                    // 2
-  // Luces pines {D:10,11} Luces superior e inferior               // 2
-  for(int i = 0 ; i<8 ;i++)ArregloActuadores[i].set(i+4);
-  // luces pines {A0,A1}   Luces Centrales                         // 2 
-  // Ventiladores {A2,A3,A4,A5} Recamara inferior                  // 4
-  for(int i = 8 ; i<14 ;i++)ArregloActuadores[i].set(i+6);
-}
+  void configurarActuadores(){
+    // Ventiladores pines {D:4,5,6,7,} Recamara Superior             // 4
+    // Ventiladores pines {D:8,9} Entrada General                    // 2
+    // Luces pines {D:10,11} Luces superior e inferior               // 2
+    for(int i = 0 ; i<8 ;i++)ArregloActuadores[i].set(i+4);
+    // luces pines {A0,A1}   Luces Centrales                         // 2 
+    // Ventiladores {A2,A3,A4,A5} Recamara inferior                  // 4
+    for(int i = 8 ; i<14 ;i++)ArregloActuadores[i].set(i+6);
+  }
--- firmware/ProgramacionESP/DHT22/DHT22.ino
+++ firmware/ProgramacionESP/DHT22/DHT22.ino
@@ -7,6 +7,9 @@
 #define Sensor1 D5
 #define Sensor2 D6
 #define Sensor3 D4
+const int pin4 = 0;
+const int pin5 = 0;
+int humedadSuelo1 = 0;
 
 // Prototipos Funciones
 void configurarDHT22();
@@ -14,7 +17,8 @@ float ValidadValor(float valor);
 float MedirHumedad(byte sensor);
 float MedirTemperatura(byte sensor);
 void imprimitHumedadYTemperatura(byte i);
-// Se inicializan los ohbjetos 
+float sensarHumedad(byte sensor);
+// Se inicializan los objetos 
 
 DHT dht1(Sensor1,DHTTYPE);
 DHT dht2(Sensor2,DHTTYPE);
@@ -23,14 +27,39 @@ DHT dht3(Sensor3,DHTTYPE);
 void setup() {
   configurarDHT22();
   Serial.begin(9600);
+  pinMode(D1,OUTPUT);
+  pinMode(D2,OUTPUT);
 }
 
 void loop() {
-  for(int i=1;i<=3;i++){
-    imprimitHumedadYTemperatura(i);
-    delay(2000);
-  }
+  for(int i=1;i<=3;i++)imprimitHumedadYTemperatura(i);
+  delay(2000);
+  humedadSuelo1 = map(analogRead(pin4),0,1024,100,0);
+
+  Serial.println("Humedad del suelo 1: " + String(sensarHumedad(1)));
+  Serial.println("Humedad del suelo 2: " + String(sensarHumedad(2)));
+     
 }
+
+float sensarHumedad( byte sensor){
+  float Humedad = 0;
+  if(sensor==1){
+    digitalWrite(D1,HIGH);
+    delay(500);
+    Humedad = map(analogRead(pin4),0,1024,100,0);
+    delay(500);
+    digitalWrite(D1,LOW);
+  }
+  else if (sensor==2){
+    digitalWrite(D2,HIGH);
+    delay(500);
+    Humedad = map(analogRead(pin4),0,1024,100,0);
+    delay(500);
+      digitalWrite(D2,LOW);
+  }
+    return Humedad ;
+}
+
 
 void imprimitHumedadYTemperatura(byte i){
   Serial.print("Sensor");
